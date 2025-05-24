d41d4a7d7df56a38fb6cf31c4477f25fc3e7ff5f
gabriellegtw <gabrielle.gianna.tan@gmail.com>
2025-05-24 12:37:09 +0800

    Initial commit

---
+++ src/main/java/com/example/ttpproject/TtpProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.ttpproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtpProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TtpProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ttpproject/TtpProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ttpproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtpProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
