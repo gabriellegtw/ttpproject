0ed590d39d88452f4b36e0d6a3074f3642b3537e
gabriellegtw <gabrielle.gianna.tan@gmail.com>
2025-05-25 10:15:08 +0800

    Add test request mapping

---
+++ src/test/java/com/example/ttpproject/HomeController.java
@@ -0,0 +1,16 @@
+package com.example.ttpproject;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+// Controllers are responsible for handling incoming web requests,
+// processing it and sending back a response
+@Controller
+public class HomeController {
+    // RequestMapping tells Spring which URL path it will respond to
+    // E.g., if the argument is "/read" it the http path will need to have it at the end
+    @RequestMapping("/")
+    public String index() {
+        // index.html is the view file
+        return "index.html";
+    }
+}
